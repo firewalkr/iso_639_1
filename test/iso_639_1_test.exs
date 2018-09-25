bd133660759998d3acf3de0ff8191e94c8bd8621
Steven Blowers <sb@rekki.com>
2018-09-25 10:40:16 +0100

    add exists?/1 function to test if values are valid ISO 639-1 codes

--- test/iso_639_1_test.exs
+++ test/iso_639_1_test.exs
@@ -1,8 +1,26 @@
 defmodule Iso639_1Test do
   use ExUnit.Case
-  doctest Iso639_1
 
-  test "greets the world" do
-    assert Iso639_1.hello() == :world
+  describe "exists?/1" do
+    test "with atoms" do
+      assert Iso639_1.exists?(:aa)
+      refute Iso639_1.exists?(:ac)
+    end
+
+    test "with strings" do
+      assert Iso639_1.exists?("aa")
+      assert Iso639_1.exists?("EN")
+      refute Iso639_1.exists?("sz")
+      refute Iso639_1.exists?("random")
+      refute Iso639_1.exists?("enen")
+    end
+
+    test "with other" do
+      refute Iso639_1.exists?(1)
+      refute Iso639_1.exists?(3.141)
+      refute Iso639_1.exists?(%{a: 1})
+      refute Iso639_1.exists?(b: 2, c: 3)
+      refute Iso639_1.exists?('aa')
+    end
   end
 end
